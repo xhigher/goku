a3c4a8e3982eb1ecc1abdc4bbaf2aece39eeabc9
王渊 <wangyuan1@xunlei.com>
2021-12-15 15:23:31 +0800

    add config start

---
+++ framework/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+type ServerConfig struct {
+	Zap   ZapConfig   `yaml:"zap"`
+	Mysql []*Mysql    `yaml:"mysql"`
+	Redis RedisConfig `yaml:"redis"`
+}
+
+type ZapConfig struct {
+	Level         string `yaml:"level"`
+	Format        string `yaml:"format"`
+	Prefix        string `yaml:"prefix"`
+	Director      string `yaml:"director"`
+	ShowLine      bool   `yaml:"showLine"`
+	EncodeLevel   string `yaml:"encode-level"`
+	StacktraceKey string `yaml:"stacktrace-key"`
+	LogInConsole  bool   `yaml:"log-in-console"`
+}
+
+type RedisConfig struct {
+	Db       int    `yaml:"db"`
+	Addr     string `yaml:"addr"`
+	Password string `yaml:"password"`
+}
--- main/main.go
+++ main/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"flag"
 	"goku.net/framework/commons"
+	"goku.net/framework/config"
 	"goku.net/framework/database"
 	"goku.net/framework/network/http"
 	"goku.net/services/user"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
+
+var (
+	configFile string
+)
+
+func init() {
+	flag.StringVar(&configFile, "c", "", "config file path")
+	flag.StringVar(&configFile, "f", "", "config file path")
+}
 
 func main() {
 
+	flag.Parse()
+	configFileContent, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+	c := config.ServerConfig{}
+	err = yaml.Unmarshal(configFileContent, &c)
+	if err != nil {
+		panic(err)
+	}
+
 	commons.InitLogger()
 
-	database.Init(configs []*config.Mysql)
+	database.Init(c.Mysql)
 
 	server := http.NewServer(8989)
 
