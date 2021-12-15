b092cf695d9533b7597210d43deed0adeda9ad43
xuchihai <xuchihai@xunlei.com>
2021-12-15 15:23:01 +0800

    UserAddressTable

--- protos/model/user.go
+++ protos/model/user.go
@@ -12,3 +12,16 @@ type UserDetailTable struct {
 func (model *UserDetailTable) TableName() string {
 	return "user_detail"
 }
+
+type UserAddressTable struct {
+	AddrId    int64  `json:"addr_id" gorm:"column:addr_id;type:bigint(20);not null;default:0"`
+	Mid       int64  `json:"mid" gorm:"column:mid;type:bigint(20);not null;default:0"`
+	Name      string `json:"name" gorm:"column:name;type:varchar(100);not null"`
+	Phoneno   string `json:"phoneno" gorm:"column:phoneno;type:varchar(50);not null"`
+	Locations string `json:"locations" gorm:"column:locations;type:varchar(200);not null"`
+	Ut        int64  `json:"ut" gorm:"column:ut;type:bigint(20);not null;default:0"`
+}
+
+func (model *UserAddressTable) TableName() string {
+	return "user_address"
+}
