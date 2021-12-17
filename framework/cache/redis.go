0cf4a353cf517f9bced50d46d94fb790caa42883
xuchihai <xuchihai@xunlei.com>
2021-12-17 11:23:43 +0800

    redis

--- framework/cache/redis.go
+++ framework/cache/redis.go
@@ -31,11 +31,13 @@ func Init(configs []*config.RedisConfig) {
 			Password: config.Password,
 			DB:       config.Db,
 		})
+
 		if client != nil {
 			redisClients[config.Name] = &RedisCache{
 				Ctx:    context.Background(),
 				client: client,
 			}
+			commons.Logger().Error("Redis连接成功", zap.Any("name", config.Name), zap.Any("addr", config.Addr))
 		} else {
 			commons.Logger().Error("Redis连接异常", zap.Any("config", config))
 			os.Exit(-1)
